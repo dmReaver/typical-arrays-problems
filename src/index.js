e9da7c4b01bdcb1ac314c92443b2c61740d7665e
Salavei Dzmitry <salavei.dv@gmail.com>
2020-02-25 23:42:39 +0300

    feat: make functions min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 
 exports.min = function min (array) {
+  if (array !== undefined) {
+    if (array.length > 0) {
+      let m = array[0];
+      array.forEach(element => {
+        if (element < m) {
+          m = element;
+        }
+      });
+      return m;
+    } else {
+      return 0;
+    }
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array !== undefined) {
+    if (array.length > 0) {
+      let ma = array[0];
+      array.forEach(element => {
+        if (element > ma) {
+          ma = element;
+        }
+      });
+      return ma;
+    } else {
+      return 0;
+    }
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array !== undefined){
+    if (array.length > 0) {
+      let sum = 0;
+      array.forEach(element => {
+        sum += element;
+      });
+      return (sum / array.length)
+    } else {
+      return 0;
+    }
+  }
   return 0;
 }
